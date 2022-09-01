4b08cef30df032629c57469c9d9d77ff5bfc6371
Raquel Behrens <raquel.behrens@hotmail.com>
2022-09-01 20:55:28 -0300

    Adicionando arquivos iniciais

---
+++ source/window.py
@@ -0,0 +1,10 @@
+from tkinter import *
+from constants import WINDOW_HEIGHT, WINDOW_WIDTH, APPLICATION_NAME
+
+root = Tk()
+root.title('Sistema Básico CG - 2D')
+#root.iconbitmap()
+root.geometry(f"{WINDOW_HEIGHT}x{WINDOW_WIDTH}")
+
+
+root.mainloop()
