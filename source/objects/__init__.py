5d69949520a59773bc07c3b521a948da76802584
Raquel Behrens <raquel.behrens@hotmail.com>
2022-11-12 14:44:08 -0300

    Arrumando exportação de ponto 3D

--- source/objects/__init__.py
+++ source/objects/__init__.py
@@ -3,6 +3,6 @@ from .line import Line
 from .wireframe import Wireframe
 from .curve import Curve
 from .object_3D import Object3D
-from .point3D import Point3D
+from .point_3D import Point3D
 from .parametric_surface_3D import ParametricSurface3D
 from .fd_surface_3D import FdSurface3D
